eb4dcd86928f653ed89c8d7d1ab718b8c5dde6e6
fatomg-jkt <fat@obsidianmanagement-group.com>
2026-08-13 08:31:44 +0700

    Mount user settings overlay

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { DashboardAuth } from "@/components/dashboard-auth";
+import { UserAccessSettingsOverlay } from "@/components/user-access-settings-overlay";
 
 export default function Home() {
-  return <DashboardAuth />;
+  return <><DashboardAuth /><UserAccessSettingsOverlay /></>;
 }
